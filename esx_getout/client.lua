3b292b8e0828e1d52fb38de658c416010a41ac68
Jare88 <73169864+Jare88@users.noreply.github.com>
2022-01-17 04:23:31 +0200

    Update client.lua

--- esx_getout/client.lua
+++ esx_getout/client.lua
@@ -15,54 +15,42 @@ Citizen.CreateThread(function()
         local ped = GetPlayerPed(-1)
         PlayerData = ESX.GetPlayerData()
         
-        if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('fibx')) then
+        if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('fibx')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
             if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
                 TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
-                TaskLeaveVehicle(ped,vehicle,0)
+                TaskLeaveVehicle(ped,vehicle,1)
             end
-		elseif
-
-             IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('policeb')) then
+		elseif IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('policeb')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
                 if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
                 TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
-                    TaskLeaveVehicle(ped,vehicle,0)
-            end
-        elseif
-
-             IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('polmav')) then
-            if PlayerData.job.name ~='police' and PlayerData.job.name ~= 'ambulance' then
-                TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
                 TaskLeaveVehicle(ped,vehicle,0)
             end
-        elseif
-
-            IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('fibj')) then
+        elseif IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('polmav')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
             if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
                 TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
                 TaskLeaveVehicle(ped,vehicle,0)
             end
-        elseif
-
-	        IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('2015polstang')) then
+        elseif IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('2015polstang')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
 			if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
 				TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla.")
 				TaskLeaveVehicle(ped,vehicle,0)
-				end
-		elseif 
-            
-            IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('fibd2')) then
-            if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
-                TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla.")
+			end
+        elseif IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('fibj')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
+            if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
+                TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
                 TaskLeaveVehicle(ped,vehicle,0)
             end
-        elseif
-
-            IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('polschafter3')) then
-            if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
-                TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla.")
+        elseif IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('fibd2')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
+            if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
+                TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
                 TaskLeaveVehicle(ped,vehicle,0)
-             
+            end
+        elseif IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), false), GetHashKey('polschafter3')) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(-1) then
+            if PlayerData.job.name ~= 'police' and PlayerData.job.name ~= 'ambulance' and PlayerData.job.name ~= 'mechanic' and PlayerData.job.name ~= 'bennys' and PlayerData.job.name ~= 'autopaja' then
+                TriggerEvent('esx:showNotification', "~r~Sinulla ei ole oikeutta ajaa virka ajoneuvolla")
+                TaskLeaveVehicle(ped,vehicle,0)
             end
 		end
 	end
+end)
 end)
